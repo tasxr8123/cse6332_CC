8672e7eec92449f82b68b66345940d998dc93c95
sr417summit <srividia_ram@yahoo.com>
2023-02-06 16:39:47 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+ACCOUNT_NAME = "cse6332"
+ACCOUNT_KEY = "6T26i2ByTpJtT1U8DqCiA8PgR78RMeYtmE3QwpRNK4To2Lm98wsz4EFy0iB5L4C82csA5yXI504D+AStvtKt4Q=="
+CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=cse6332;AccountKey=6T26i2ByTpJtT1U8DqCiA8PgR78RMeYtmE3QwpRNK4To2Lm98wsz4EFy0iB5L4C82csA5yXI504D+AStvtKt4Q==;EndpointSuffix=core.windows.net"
+CONTAINER = "csv"
+#ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg','csv'])
+#MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
